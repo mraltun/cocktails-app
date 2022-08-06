21815bd79b9496aa81f8bbfe6a66cc9981beeb9a
Murat Altun <mraltun@gmail.com>
2022-08-06 13:08:09 +0300

    Added components boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCocktail from "./pages/SingleCocktail";
+import Error from "./pages/Error";
 
 const App = () => {
   return <div>App</div>;
