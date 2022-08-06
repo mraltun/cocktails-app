817f1bceb1f4508b39e800fda059b080af4c14ba
Murat Altun <mraltun@gmail.com>
2022-08-06 13:49:50 +0300

    About and Error pages are done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quia saepe
+        ex laboriosam, repellat dolores, iure pariatur necessitatibus maiores
+        cum quibusdam nostrum ad deleniti quam aliquam veniam, beatae
+        accusantium facilis. Laudantium id tempora hic ad optio consequuntur
+        aliquam aliquid nesciunt. Voluptates quibusdam modi, eius ratione dicta
+        consequuntur cumque accusantium dolore?
+      </p>
+    </section>
+  );
 };
 
 export default About;
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <section className='error-page section'>
+      <div className='error-container'>
+        <h1>page is not found!</h1>
+        <Link to='/' className='btn btn-primary'>
+          back home
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default Error;
